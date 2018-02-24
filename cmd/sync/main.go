e16bb4600779475ee00be3a42efb42f59df6a676
jdorman <john.dorman@sony.com>
2018-02-24 00:59:49 -0800

    Updating version

--- cmd/sync/main.go
+++ cmd/sync/main.go
@@ -19,7 +19,7 @@ import (
 
 var configFile = flag.String("config_path", "/etc/nginx/aws.yaml", "Path to the config file")
 var logFile = flag.String("log_path", "", "Path to the log file. If the file doesn't exist, it will be created")
-var version = "0.1-2"
+var version = "0.2-0"
 
 const connTimeoutInSecs = 10
 

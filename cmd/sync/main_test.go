b1943a67630a80cca7c8d10b4ec0287f5aef07bc
Vighneswar Rao Bojja <v.bojja@f5.com>
2019-10-04 15:48:44 +0530

    Fix format

--- cmd/sync/main_test.go
+++ cmd/sync/main_test.go
@@ -6,13 +6,13 @@ func TestSetPositiveInt(t *testing.T) {
 	defaultValue := 0
 	value := setPositiveInt(10, defaultValue)
 	if value == 0 {
-		t.Errorf(" setPositiveInt() should return value %v but returned invalid value %v", value, defaultValue)
+		t.Errorf("setPositiveInt() should return value %v but returned invalid value %v", value, defaultValue)
 	}
 
 	defaultValue = 1
 	value = setPositiveInt(0, defaultValue)
 	if value != 1 {
-		t.Errorf(" setPositiveInt() should return default value %v but returned invalid value %v", defaultValue, value)
+		t.Errorf("setPositiveInt() should return default value %v but returned invalid value %v", defaultValue, value)
 	}
 }
 
@@ -21,14 +21,14 @@ func TestSetTime(t *testing.T) {
 	time := setTime("20s", defaultTime)
 
 	if time != "20s" {
-		t.Errorf(" setTime() should return time %v but returned invalid time %v", time, defaultTime)
+		t.Errorf("setTime() should return time %v but returned invalid time %v", time, defaultTime)
 	}
 
 	defaultTime = "10s"
 	time = setTime("", defaultTime)
 
 	if time != "10s" {
-		t.Errorf(" setTime() should return default time %v but returned invalid time %v", defaultTime, time)
+		t.Errorf("setTime() should return default time %v but returned invalid time %v", defaultTime, time)
 	}
 }
 
@@ -37,12 +37,12 @@ func TestSetPositiveIntOrZeroFromPointer(t *testing.T) {
 	v := 10
 	value := setPositiveIntOrZeroFromPointer(&v, defaultValue)
 	if value == 1 {
-		t.Errorf(" setPositiveIntOrZeroFromPointer() should return value %v but returned invalid value %v", value, defaultValue)
+		t.Errorf("setPositiveIntOrZeroFromPointer() should return value %v but returned invalid value %v", value, defaultValue)
 	}
 
 	defaultValue = 1
 	value = setPositiveIntOrZeroFromPointer(nil, defaultValue)
 	if value != 1 {
-		t.Errorf(" setPositiveIntOrZeroFromPointer() should return default value %v but returned invalid value %v", defaultValue, value)
+		t.Errorf("setPositiveIntOrZeroFromPointer() should return default value %v but returned invalid value %v", defaultValue, value)
 	}
 }
